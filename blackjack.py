a0732f6ffd168ce7178a3652be17572098a1e68d
Riley Howsden <42232154+rbhowsden@users.noreply.github.com>
2021-05-20 06:04:58 -0700

    Basic Updates - restarting
    new updates

--- blackjack.py
+++ blackjack.py
@@ -1,13 +1,18 @@
 import random
 
+class Card:
+    def __init__(self, value):
+        self.value = value
+
 class Shoe:
-    def __init__(self, decks=6, penetration=.75,
-                 ranks=['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']):
+    def __init__(self, decks=6, penetration=.75):
         self.decks = decks
         self.penetration = penetration
-        self.ranks = ranks
-        self.cards = ranks*decks*4
-        #Problem here is that it only reshuffles what is currently in the shoe
+        self.ranks = [Card(v) for v in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]]
+        self.history = {"A": 0, "2": 0, "3": 0, "4": 0,
+                        "5": 0, "6": 0, "7": 0, "8": 0,
+                        "9": 0, "10": 0, "J": 0, "Q": 0, "K": 0}
+        self.cards = 4*self.ranks*self.decks
         self.size = len(self.cards)
         self.shuffle()
 
@@ -15,22 +20,45 @@ class Shoe:
         random.shuffle(self.cards)
 
     def pull_card(self):
-        #Basically will need a trigger that tells to reshuffle
-        return self.cards.pop(0)
+        new_card = self.cards.pop(0)
+        self.history[new_card.value] += 1
+        return new_card.value
 
+class Hand:
+    def __init__(self, split=None):
+        if split:
+            self.cards = [split]
+        else:
+            self.cards = []
+        while len(self.cards) < 2:
+            self.cards.append(card)
+        self.value = 0
+        self.calculate_value()
+        self.soft = False
 
+    def hit(self):
+        self.cards.append(card)
 
+    def split(self):
+        return Hand(self.cards[0]), Hand(self.cards[0])
 
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+        for card in self.cards:
+            if card.value.isnumeric():
+                self.value += int(card.value)
+            else:
+                if card.value == "A":
+                    has_ace = True
+                    self.value += 11
+                else:
+                    self.value += 10
 
+        if has_ace and self.value > 21:
+            self.value -= 10
 
-
-import random
-
-ranks=['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-decks=7
-penetration=.75
-
-def shuffle
-
-
-https://github.com/suhasgaddam/blackjack-python/tree/master/blackjack
+class Game:
+    self.shoe = Shoe()
+    self.p_hand = Hand()
+    self.d_hand = Hand()
